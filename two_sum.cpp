9992dfff349020285a3f3f55c7ac929c8f0f79e1
pulkit-first <goyalpulkit719@gmail.com>
2026-03-12 18:23:31 +0530

    Initial commit

---
+++ two_sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;  // value -> index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (m.count(complement)) {
+                return {m[complement], i};
+            }
+
+            m[nums[i]] = i;  // store current number's index
+        }
+
+        return {};  
+    }
+};
